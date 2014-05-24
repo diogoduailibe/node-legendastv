c187f64ce2fa2dc8b48bcfcb40261a32302a9be1
Diogo Duailibe <dioduailibe@gmail.com>
2014-05-24 18:37:30 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# node-legendastv
+ 
+Node.js client for popular brazilian subtitle site [legendas.tv](http://www.legendas.tv).
+
+## Install
+
+```sh
+$ npm install node-legendastv
+```
+
+## API
+
+```js
+var legendastv = require('legendastv')
+```
+
+### search(termo, opts, cb)
+
+- `termo` a string used for find series. This is required. Ex: "game of thrones", "the mentalist S06E01", etc.
+- `opts` an object with the following arguments:
+  - `idioma` a integer indicating the subtitle language. Could be one of the following:
+    - `1`  Portuguese-BR
+    - `2`  English
+    - `3`  Spanish
+    - `4`  French
+    - `5`  German
+    - `6`  Japanese
+    - `7`  Danish
+    - `8`  Norwegian
+    - `9`  Swedish
+    - `10` Portuguese-PT
+    - `11` Arabic
+    - `12` Czech
+    - `13` Chinese
+    - `14` Korean
+    - `15` Bulgarian
+    - `16` Italian
+    - `17` Polish
+  
+  - `tipo_legenda` a string indicating the subtitle type. Could be one of the following:
+    - `d` Destaque
+    - `p` Pack
+  - `proxy` a string representing a HTTP proxy to be used.
+  
+
+## Example
+
+```js
+var legendastv = require('legendastv');
+
+legendastv.search('game of thrones S04E01', function(err, results) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(results);
+  }
+});
+
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,172 @@
+var validator = require('validator')
+    ,cheerio = require('cheerio')
+    ,async = require('async')
+    ,request = require('request')
+    ,baseUrl = 'legendas.tv'
+    ,protocol = "http://"
+    ,baseSearchUrl = protocol + baseUrl + '/util/carrega_legendas_busca/'
+    ,baseUrlDownload = protocol + baseUrl + '/downloadarquivo/';
+
+/*
+    opts:
+        - idioma:
+                undefined - all
+                1 - Português-BR
+                2 - Inglês
+                3 - Espanhol
+                4 - Francês
+                5 - Alemão
+                6 - Japonês
+                7 - Dinamarquês
+                8 - Norueguês
+                9 - Sueco
+                10 - Português-PT
+                11 - Árabe
+                12 - Checo
+                13 - Chinês
+                14 - Coreano
+                15 - Búlgaro
+                16 - Italiano
+                17 - Polonês
+        - tipo_legenda:
+                undefined - all
+                d - destaque
+                p - pack
+
+        - proxy:
+                protocol://IP_OR_NAME[:PORT]
+
+ */
+
+exports.search = function(termo, opts, cb) {
+    var url = ''+baseSearchUrl;
+
+    if (opts && typeof opts === 'function'){
+        cb = opts;
+        opts = {};
+    }
+
+    opts = opts || {};
+
+    if (termo && !validator.isNull(termo) && termo!==''){
+        url += '/' + termo;
+    }
+    else{
+        return cb(new Error('termo is invalid'));
+    }
+
+    if (opts && opts!== null && opts.idioma){
+        var idioma = validator.toInt(opts.idioma,10);
+        if (idioma!== NaN && idioma > 0 && idioma < 18){
+            url += '/' + idioma;
+        }
+        else{
+            return cb(new Error('idioma is invalid'));
+        }
+    }
+    else{
+        url += '/-';
+    }
+
+    if (opts && opts!== null && opts.tipo_legenda){
+        var tipo_legenda = opts.tipo_legenda;
+        if (!validator.isNull(tipo_legenda) && (tipo_legenda === 'd' || tipo_legenda === 'p')){
+            url += '/' + tipo_legenda;
+        }
+        else{
+            return cb(new Error('tipo_legenda is invalid'));
+        }
+    }
+    else{
+        url += '/-';
+    }
+
+    if (opts && opts!== null && opts.proxy){
+         var proxy = opts.proxy;
+        if (!validator.isURL(proxy))
+            return cb(new Error('proxy is invalid'));
+    }
+
+    url = encodeURI(url);
+
+    return parsePage(url, termo, opts, parseResults, cb);
+};
+
+function parsePage(url, termo, opts, parse, cb) {
+    var query = {
+        url:url
+    };
+
+    if (opts && opts!== null && opts.proxy){
+        query.proxy = opts.proxy;
+    }
+
+    request(query,function(err,resp,body){
+        if (!err){
+            parse(termo,body,opts,cb);
+        }
+        else cb(err);
+    });
+
+};
+
+function parseResults(termo,resultsHTML,opts, cb) {
+    var $ = cheerio.load(resultsHTML)
+        ,arrayLinks = []
+        ,undef;
+
+    $('.f_left').each(function(index){
+        var subtitle = {};
+        var link = $(this).find('p').first().find('a');
+        subtitle.link = link;
+        subtitle.parent = $(this).parent();
+        arrayLinks.push(subtitle);
+    });
+
+    async.mapSeries(arrayLinks,function(item,cb){
+
+        var href = $(item.link).attr('href'),
+            text = $(item.link).text().toUpperCase(),
+            search = termo.replace(/\s/g,'.').toUpperCase()
+
+
+        if (text.indexOf(search)!== -1){
+            var split = href.split('/');
+            item.linkLegenda = baseUrlDownload +split[2];
+            item.link = protocol+baseUrl + href;
+            item.titulo = text;
+            item.destaque = ($(item.parent).hasClass('destaque')?1:0);
+            item.idioma = $(item.parent).find('img').attr('title');
+            delete item.parent;
+
+            var query = {
+                url:item.link
+            };
+
+            if (opts && opts!== null && opts.proxy){
+                query.proxy = opts.proxy;
+            }
+            request(query,function(err,resp,body){
+                if (!err){
+                    $1 = cheerio.load(body);
+                    item.upload = {};
+                    item.upload.data = $1('.date').text();
+                    item.upload.hora = $1('.hour').text();
+                    item.upload.usuario = $1('.nume > a').text();
+                    item.numero_downloads = $1('.number').text();
+
+                    cb(undef,item);
+                }
+                else cb(err);
+            });
+        }
+    },function(err,results){
+        if (!err) {
+            results = results.filter(function (item) {
+                return (typeof item.linkLegenda !== 'undefined');
+            });
+            cb(undef,results);
+        }
+    });
+};
+
